caa007ae47fb3cd581792a525842871069f06cb3
Kristine Gaprindashvili <kristigaphrindashvili@gmail.com>
2025-04-10 19:55:08 +0400

    minor changes

--- invoice_generator_api/settings.py
+++ invoice_generator_api/settings.py
@@ -165,6 +165,12 @@ CORS_ALLOW_METHODS = [
     "OPTIONS",
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
